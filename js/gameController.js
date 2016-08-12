a6452e51611a0835f212a57cfef3a3e2c268e334
Charlie Furniss <charliefurniss96@gmail.com>
2016-08-12 14:31:59 +0100

    adding and registering rock, paper, scissors buttons

--- js/gameController.js
+++ js/gameController.js
@@ -10,5 +10,23 @@ function GameController(){
 		console.log("start game");
 	
 	}
+
+	this.selectRock = function(){
+
+		console.log("rock");
+	
+	}
+
+	this.selectPaper = function(){
+
+		console.log("paper");
+	
+	}
+
+	this.selectScissors = function(){
+
+		console.log("scissors");
+	
+	}
 	
 }
